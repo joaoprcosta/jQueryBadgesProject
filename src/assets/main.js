2b1307e20bed6c998079c224d1b1639f6d552645
Joao Costa <joaocosta@MacintoshdeJoao.lan>
2017-02-22 21:55:41 +0000

    Json request

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://github.com/users/joaoprcosta.json',
+        dataType: 'jsonp',
+        success: function(response) {
+            return "We made a sample Ajax request available to help you with this task.";
+        }
+    });
 
 });
