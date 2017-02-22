749d5275647a200051a764581cda3674a0bfe09b
Joao Costa <joaocosta@MacintoshdeJoao.lan>
2017-02-22 23:17:43 +0000

    Project Finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,36 @@
 $(function() {
 
     $.ajax({
-        url: 'https://github.com/users/joaoprcosta.json',
+        url: 'https://www.codeschool.com/users/joaoprcosta.json',
         dataType: 'jsonp',
         success: function(response) {
             return "We made a sample Ajax request available to help you with this task.";
         }
-    });
+    })
+    .done(function(response){
+        console.log("response", response.courses.completed);
+        function addCourses(courses){
+            var badges = $("#badges");
+            courses.forEach(function(course){
+                var $course = $('<div />').addClass('course').appendTo('#badges');
+                
+                $('<h3 />', {
+                    text: course.title
+                }).appendTo($course);
+                
+                $('<img />', {
+                    src: course.badge
+                }).appendTo($course);
+                
+                $('<a />', {
+                    href: course.url,
+                    target: "_blank",
+                    'class': 'btn btn-primary',
+                    text: "See Course"
+                }).appendTo($course);
+            })
+        };
+        addCourses(response.courses.completed);
+    })
 
 });
